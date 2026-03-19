0c2d8c5d3b0b9ee33d8bedf744cb182267868269
balazs.czibulka <balazs.czibulka@egroup.hu>
2026-03-19 18:56:58 +0100

    deletes docker frontend

--- frontend/docker-frontend/postgres-setup.sh
+++
@@ -1,8 +0,0 @@
-echo "Checking if keycloak_db exists..."
-if ! psql -h postgres -U admin -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'keycloak_db'" | grep -q 1; then
-    echo "Database not found. Creating keycloak_db..."
-    psql -h postgres -U admin -d postgres -c "CREATE DATABASE keycloak_db;"
-else
-    echo "keycloak_db already exists. Skipping creation."
-fi
-echo "keycloak_db setup complete."
